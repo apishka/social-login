7d77a3996b295211be45a70c4c1bc5003ae00b93
Alex Lobtsov <dessander@gmail.com>
2014-09-25 15:23:15 +0400

    initialize

--- source/Apishka/ServiceLogin/ServiceLogin.php
+++ source/Apishka/ServiceLogin/ServiceLogin.php
@@ -1,1 +1,62 @@
 <?php
+
+/**
+ * Apishka social login main class
+ *
+ * @author  Alex "grevus" Lobtsov <alex@lobtsov.com>
+ */
+
+class Apishka_SocialLogin_SocialLogin
+{
+    /**
+     * Config
+     *
+     * @var array
+     * @access private
+     */
+
+    private $_config = null;
+
+    /**
+     * Construct
+     *
+     * @param array     $config
+     * @access public
+     * @return void
+     */
+
+    public function __construct(array $config)
+    {
+        $this->initialize($config);
+    }
+
+    /**
+     * Initialize
+     *
+     * @param array     $config
+     * @access protected
+     * @return Apishka_SocialLogin_SocialLogin this
+     */
+
+    protected function initialize(array $config)
+    {
+        $this->_config = array_replace_recursive(
+            $this->_getConfigFilePath(),
+            $config
+        );
+
+        return $this;
+    }
+
+    /**
+     * Returns path to config file
+     *
+     * @access protected
+     * @return string
+     */
+
+    protected function _getConfigFilePath()
+    {
+        return realpath(__DIR__ . '/../../../storage/config.php');
+    }
+}
