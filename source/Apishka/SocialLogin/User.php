1738eb3dba8717f776f7131f25b3c85b76b90c54
Alex Lobtsov <dessander@gmail.com>
2014-11-02 11:39:25 +0400

    Change logic for user data

--- source/Apishka/SocialLogin/User.php
+++ source/Apishka/SocialLogin/User.php
@@ -1,16 +1,7 @@
 <?php
 
 /**
- * Apishka social login user
- *
- * Fields:
- *  id
- *  fullname
- *  login
- *  email
- *  gender
- *  avatar
- *  birthdate
+ * Apishka social login user model
  *
  * @author  Alex "grevus" Lobtsov <alex@lobtsov.com>
  */
@@ -21,9 +12,9 @@ class Apishka_SocialLogin_User
      * Constants
      */
 
-    const GENDER_ALL    = 0;
-    const GENDER_FEMALE = 1;
-    const GENDER_MALE   = 2;
+    const GENDER_NONE   = 'none';
+    const GENDER_FEMALE = 'female';
+    const GENDER_MALE   = 'male';
 
     /**
      * Data
@@ -35,70 +26,240 @@ class Apishka_SocialLogin_User
     private $_data = array();
 
     /**
+     * Normalized
+     *
+     * @var array
+     * @access private
+     */
+
+    private $_normalized = array();
+
+    /**
      * Construct
      *
      * @access public
      * @return void
      */
 
-    public function __construct(array $data = array())
+    public function __construct(array $data = array(), array $normalized = array())
     {
-        $this->_data = $data;
+        $this->_data        = $data;
+        $this->_normalized  = $normalized;
     }
 
     /**
-     * Magic get
+     * Sets id
      *
-     * @param string    $name
+     * @param string    $id
      * @access public
-     * @return mixed
+     * @return Apishka_SocialLogin_User this
      */
 
-    public function __get($name)
+    public function setId($id)
     {
-        return $this->_data[$name];
-    }
-
-    /**
-     * Set
-     *
-     * @param string    $name
-     * @param mixed     $value
-     * @access public
-     * @return Apishka_SocialLogin_User
-     */
-
-    public function __set($name, $value)
-    {
-        $this->_data[$name] = $value;
+        $this->_normalized['id'] = (string) $id;
 
         return $this;
     }
 
     /**
-     * Get
+     * Returns id
      *
-     * @param string    $name
      * @access public
-     * @return mixed
+     * @return string
      */
 
-    public function get($name)
+    public function getId()
     {
-        return $this->__get($name);
+        return $this->_normalized['id'];
     }
 
     /**
-     * Set
+     * Sets fullname
      *
      * @param string    $name
-     * @param mixed     $value
      * @access public
-     * @return Apishka_SocialLogin_User
+     * @return Apishka_SocialLogin_User this
      */
 
-    public function set($name, $value)
+    public function setFullname($name)
     {
-        return $this->__set($name, $value);
+        $this->_normalized['fullname'] = $name;
+
+        return $this;
+    }
+
+    /**
+     * Returns fullname
+     *
+     * @access public
+     * @return string
+     */
+
+    public function getFullname()
+    {
+        return $this->_normalized['fullname'];
+    }
+
+    /**
+     * Set login
+     *
+     * @param string    $login
+     * @access public
+     * @return Apishka_SocialLogin_User this
+     */
+
+    public function setLogin($login)
+    {
+        $this->_normalized['login'] = $login;
+
+        return $this;
+    }
+
+    /**
+     * Returns login
+     *
+     * @access public
+     * @return string
+     */
+
+    public function getLogin()
+    {
+        return $this->_normalized['login'];
+    }
+
+    /**
+     * Set email
+     *
+     * @param string    $email
+     * @access public
+     * @return Apishka_SocialLogin_User this
+     */
+
+    public function setEmail($email)
+    {
+        $this->_normalized['email'] = $email;
+
+        return $this;
+    }
+
+    /**
+     * Returns email
+     *
+     * @access public
+     * @return string
+     */
+
+    public function getEmail()
+    {
+        return $this->_normalized['email'];
+    }
+
+    /**
+     * Set gender
+     *
+     * @param string    $gender
+     * @access public
+     * @return Apishka_SocialLogin_User this
+     */
+
+    public function setGender($gender)
+    {
+        $this->_normalized['gender'] = $gender;
+
+        return $this;
+    }
+
+    /**
+     * Returns gender
+     *
+     * @access public
+     * @return string
+     */
+
+    public function getGender()
+    {
+        return $this->_normalized['gender'];
+    }
+
+    /**
+     * Set avatar
+     *
+     * @param string    $avatar
+     * @access public
+     * @return Apishka_SocialLogin_User this
+     */
+
+    public function setAvatar($avatar)
+    {
+        $this->_normalized['avatar'] = $avatar;
+
+        return $this;
+    }
+
+    /**
+     * Returns avatar
+     *
+     * @access public
+     * @return string
+     */
+
+    public function getAvatar()
+    {
+        return $this->_normalized['avatar'];
+    }
+
+    /**
+     * Set birthday
+     *
+     * @param string    $birthday
+     * @access public
+     * @return Apishka_SocialLogin_User this
+     */
+
+    public function setBirthday($birthday)
+    {
+        $this->_normalized['birthday'] = $birthday;
+
+        return $this;
+    }
+
+    /**
+     * Returns birthday
+     *
+     * @access public
+     * @return datetime
+     */
+
+    public function getBirthday()
+    {
+        return $this->_normalized['birthday'];
+    }
+
+    /**
+     * Set data
+     *
+     * @param array     $data
+     * @access public
+     * @return Apishka_SocialLogin_User this
+     */
+
+    public function setData(array $data)
+    {
+        $this->_data = $data;
+
+        return $this;
+    }
+
+    /**
+     * Returns original data
+     *
+     * @access public
+     * @return array
+     */
+
+    public function getData()
+    {
+        return $this->_data;
     }
 }
