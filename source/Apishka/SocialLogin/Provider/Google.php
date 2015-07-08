d9e16916da6614d80b60849a31a4bab79faa422e
Alex Lobtsov <dessander@gmail.com>
2015-07-08 22:39:22 +0400

    Fix

--- source/Apishka/SocialLogin/Provider/Google.php
+++ source/Apishka/SocialLogin/Provider/Google.php
@@ -72,6 +72,9 @@ class Apishka_SocialLogin_Provider_Google extends Apishka_SocialLogin_Provider_O
 
     protected function getOauthScope()
     {
+        if ($this->getProviderConfig()['scope'])
+            return $this->getProviderConfig()['scope'];
+
         return 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/plus.profile.emails.read';
     }
 
